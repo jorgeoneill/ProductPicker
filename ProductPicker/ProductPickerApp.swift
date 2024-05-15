cde0ca0661655377cef3e77eb1b5dc177f53bde1
Jorge O'Neill <jorge.oneill@me.com>
2024-05-15 18:16:40 +0100

    Initial Commit

---
+++ ProductPicker/ProductPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductPickerApp.swift
+//  ProductPicker
+//
+//  Created by Jorge O'Neill on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
